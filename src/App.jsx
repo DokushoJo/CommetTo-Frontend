0ef58ca9cb77a697c222cf935150771b27e8e79b
karl komeya <karlkomeya04@gmail.com>
2024-08-02 15:12:37 +0900

    feat: Implemented functionality for add event (Dialog box) on main page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 // @ts-ignore
 import LeftSide from './components/LeftSide';
 import RightSide from './components/RightSide';
+import { Save } from './components/Save';
+import { Edit } from './components/Edit';
 
 const BACKEND_URL = import.meta.env.VITE_APP_BASE_URL;
 
 function App() {
-	const [count, setCount] = useState(0)
+	
+	// ADD BUTTON CLICK
+	const [dialogContent, setDialogContent] = useState(null);
+	const dialogRef = useRef(null);
+
+	const toggleAdd = () =>  {
+		if(!dialogRef.current){
+			return;
+		}
+		dialogRef.current.hasAttribute("open") ? dialogRef.current.close() 
+		: dialogRef.current.showModal();
+	};
+
 
 	return (
 		<>
+			
 			<div>
 				<div className='flex'>
 					<div className='h-screen'>
@@ -18,11 +33,25 @@ function App() {
 						<div className='absolute flex settings--width-height bottom-0 text-white tile-bg'>
                     		<div className='m-auto'>Settings</div>
 							<div className='m-2 h-20 float-right rounded-lg tile-inner-bg'>
-                        		<div className='m-2'>Add Event Button</div>
+
+								{/* Add Event Button Dialog */}
+                        		<div className='m-2'>
+									<button onClick={()=>{
+										setDialogContent(<Save />)
+										toggleAdd()	}}> ADD EVENT </button>
+
+									<dialog className='rounded-lg' ref={dialogRef} onClick={(e) => {
+										if(e.currentTarget === e.target){
+											toggleAdd(); }}}>
+												{dialogContent}</dialog>
+								</div>
                     		</div>
                 		</div>
 					</div>
+					
+					
 					<RightSide/>
+					
 	  			</div>
 			</div>
 		</>
