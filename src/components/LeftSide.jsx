5d751885cd4c9482d85dfbaf88d61da597f187a4
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-02 14:12:28 +0900

    Merge manually with spencer branches' code

---
+++ src/components/LeftSide.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import "./LeftSide.css"
+import  ListEvents  from './ListEvents'
+import SearchBox from './SearchBox'
+
+export default function LeftSide() {
+  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState("")
+
+    return (
+        <>
+            <div className='h-screen flex-col overflow-auto leftside--width bg-black text-white'>
+                Left Side
+                <div className='m-5 mb-20 mt-20 pb-1 rounded-lg tile-bg'>
+                    <div className='m-2'>Search Box</div>
+                    <div className='m-2 rounded-lg tile-inner-bg'>Search</div>
+                    <SearchBox {...{inputText, setInputText}}></SearchBox>
+                </div>
+                <div className='m-5 pb-0.5 rounded-lg tile-bg'>
+                    <div className='m-2'>Events Box</div>
+                    <div className='event-box--height-text-size overflow-auto m-2 rounded-lg tile-inner-bg'>Events</div>
+                </div>
+                <ListEvents {...{inputText}}/>
+            </div>
+        </>
+    )
+}
