91f7308dbff7de74472daf43f20606990c67be65
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-03 19:46:39 +0900

    Temporalily add add add button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LeftSide from "./components/LeftSide"
 import RightSide from "./components/RightSide"
 import TimeSchedule from "./components/TimeSchedule";
 import TimeScheduleList from "./components/TimeScheduleList";
-import { Save } from "./components/Save";
+import AddNewEvent from "./components/AddNewEvent";
 
 const BACKEND_URL = import.meta.env.VITE_APP_BASE_URL;
 
@@ -13,21 +13,22 @@ function App() {
 
 	return (
 		<>
-			{/* <div>
+			<div>
 				<div className="flex">
 					<div className="h-screen">
 						<LeftSide />
 						<div className="absolute flex settings--width-height bottom-0 text-white tile-bg">
 							<div className="m-auto">Settings</div>
 							<div className="m-2 h-20 float-right rounded-lg tile-inner-bg">
-								<div className="m-2">Add Event Button</div>
+								<div className="m-2">Add Event Button
+									<AddNewEvent></AddNewEvent>
+								</div>
 							</div>
 						</div>
 					</div>
 					<RightSide />
 				</div>
-			</div> */}
-			<Save></Save>
+			</div>
 		</>
 	);
 }
