0cb86229e7027b6427580161f7dbb575ed63ae3a
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-03 19:36:51 +0900

    Fix: Header auth

--- src/util/util.jsx
+++ src/util/util.jsx
@@ -1,7 +1,17 @@
 export const sessionData = () => {
     return {
 			id: window.localStorage.getItem("id"),
-			token: window.localStorage.getItem("token"),
+			token: window.localStorage.getItem("token").replace('"',"").replace('"',""),
 		};
 
+}
+
+export const setHeader = (method, body) => {
+	return {
+		headers: {
+			Authorization: sessionData().token,
+		},
+		method: method,
+		body: JSON.stringify(body),
+	};
 }
