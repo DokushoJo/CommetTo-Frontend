2468d540fb610e5212c098cfdf43688ef30daa8f
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-04 18:41:41 +0900

    Organized some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState} from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import LeftSide from './components/LeftSide';
 import RightSide from './components/RightSide';
@@ -6,7 +6,7 @@ import { Save } from './components/Save';
 import { Edit } from './components/Edit';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
-import {sessionData} from "./util/util";
+import { sessionData } from "./util/util";
 import AddNewEvent from './components/AddNewEvent';
 
 const BACKEND_URL = import.meta.env.VITE_APP_BASE_URL;
@@ -16,12 +16,12 @@ function App() {
 
 	// ADD BUTTON CLICK
 	const dialogRef = useRef(null);
-	const toggleAdd = () =>  {
-		if(!dialogRef.current){
+	const toggleAdd = () => {
+		if (!dialogRef.current) {
 			return;
 		}
-		dialogRef.current.hasAttribute("open") ? dialogRef.current.close() 
-		: dialogRef.current.showModal();
+		dialogRef.current.hasAttribute("open") ? dialogRef.current.close()
+			: dialogRef.current.showModal();
 	};
 
 	// LIST 
@@ -32,45 +32,48 @@ function App() {
 	//UseState
 	const [dialogContent, setDialogContent] = useState(null); //add button click
 	const [register, setRegister] = useState('login'); //switch form log in to register if no user account
-	const [selectedEventId, setSelectedEventId] = useState(null)	
+	const [selectedEventId, setSelectedEventId] = useState(null)
 
 	return (
 		<>
-		
-		{/* //function to click register new user and already have an account 
+
+			{/* //function to click register new user and already have an account 
 		{register === 'login' ? (<Login FormHandle={setRegister}/> ) : (<Register FormHandle={setRegister}/>)};
 		*/}
-		
-			 <div>
+
+			<div>
 				<div className='flex'>
 					<div className='h-screen'>
-						<LeftSide sendEventIdToRightSide={sendEventIdToRightSide}/>
+						<LeftSide sendEventIdToRightSide={sendEventIdToRightSide} />
 						<div className='absolute flex settings--width-height bottom-0 text-white tile-bg'>
-                    		<div className='m-auto'>Settings</div>
+							<div className='m-auto'>Settings</div>
 							<div className='m-2 h-20 float-right rounded-lg tile-inner-bg'>
 
 
 								{/* Add Event Button Dialog box */}
 								<div >
-									<button onClick={()=>{
-									setDialogContent(<AddNewEvent />)
-									toggleAdd()	}}> ADD EVENT </button>
+									<button onClick={() => {
+										setDialogContent(<AddNewEvent />)
+										toggleAdd()
+									}}> ADD EVENT </button>
 
 									<dialog className='rounded-lg' ref={dialogRef} onClick={(e) => {
-									if(e.currentTarget === e.target){
-										toggleAdd(); }}}>
+										if (e.currentTarget === e.target) {
+											toggleAdd();
+										}
+									}}>
 										{dialogContent}</dialog>
 								</div>
-                    		</div>
-                		</div>
+							</div>
+						</div>
 					</div>
-					<RightSide selectedEventId={selectedEventId}/>
-	  			</div>
-			</div> 
+					<RightSide selectedEventId={selectedEventId} />
+				</div>
+			</div>
 		</>
-	
 
-		)	
+
+	)
 }
 
 export default App;
